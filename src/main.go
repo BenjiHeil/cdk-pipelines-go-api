aa191cd2cfaaa13da398fd1397bae6cea71a8da7
BenjiHeil <ben.heil.dev@gmail.com>
2023-07-24 10:45:21 -0400

    fix things

--- src/main.go
+++ src/main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"strconv"
-	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -74,7 +73,7 @@ func (d *DivideByZeroError) Error() string {
 }
 
 func Divide(x, y int) (int, error) {
-	time.Sleep(2000)
+	//time.Sleep(2000)
 	if y == 0 {
 		return 0, &DivideByZeroError{params: map[string]int{"x": x, "y": y}}
 	}
